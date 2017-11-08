1dfb5b78a54b86761a50cf55ceadad1258139692
zealfire <iamalok185@gmail.com>
2017-11-08 14:19:09 +0530

    correcting automate login script with description about kcpassword

--- auto_login.sh
+++ auto_login.sh
@@ -1,4 +1,14 @@
 #/bin/bash
-username=`whoami`
-sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser $username
+username=$(whoami)
+# we need to create this file before actually running this script
+# To create this file follow these steps:
+# * Enable automate login from UI
+# * The above step creates kcpassword file in /etc location
+# * copy the above created file to a location of your choice, this location you have to use in file_name variable
+file_name='/Users/zealfire/Documents/kcpassword'
+if [ -e "$file_name" ]
+then
+    sudo cp -r $file_name /etc/
+	sudo defaults write /Library/Preferences/com.apple.loginwindow autoLoginUser $username
+fi
 exit 0
