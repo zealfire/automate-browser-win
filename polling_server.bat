00c9367b91f2c63e6a3a6afda8da14184161b9d5
zealfire <iamalok185@gmail.com>
2017-11-10 14:36:41 +0000

    added script for keep running server

---
+++ polling_server.bat
@@ -0,0 +1,14 @@
+:loop
+echo.printed
+FOR /F "tokens=5" %%i in ('netstat -aon ^| findstr 8003') do SET PID=%%i
+if "%PID%"=="" (CALL :start_server)
+ping localhost -n 11 > null
+goto loop
+
+:start_server
+echo testing
+set PATH_TO_RUBY=C:\Ruby24-x64\bin
+set RUBY=%PATH_TO_RUBY%\ruby.exe
+set PATH_TO_SCRIPTS=C:\Users\Administrator\Desktop\automate-browser-win
+%RUBY% %PATH_TO_SCRIPTS%\service.rb
+EXIT /B 0
