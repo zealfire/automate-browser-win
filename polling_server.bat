44e5aec360547029d62fe618b1e4cdf076b72270
zealfire <iamalok185@gmail.com>
2017-11-10 15:07:14 +0000

    asynchronously making call to start server

--- polling_server.bat
+++ polling_server.bat
@@ -2,6 +2,7 @@
 echo.printed
 FOR /F "tokens=5" %%i in ('netstat -aon ^| findstr 8003') do SET PID=%%i
 if "%PID%"=="" (CALL :start_server)
+SET PID=
 ping localhost -n 11 > null
 goto loop
 
@@ -10,5 +11,5 @@ echo testing
 set PATH_TO_RUBY=C:\Ruby24-x64\bin
 set RUBY=%PATH_TO_RUBY%\ruby.exe
 set PATH_TO_SCRIPTS=C:\Users\Administrator\Desktop\automate-browser-win
-%RUBY% %PATH_TO_SCRIPTS%\service.rb
+start %RUBY% %PATH_TO_SCRIPTS%\service.rb
 EXIT /B 0
